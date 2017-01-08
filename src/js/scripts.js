db5ddbdc5f9aa6e260ab22c692fc8f1fd200e96e
Michiel <linearza@users.noreply.github.com>
2017-01-08 17:44:57 +0200

    Adds contact form and more content formatting

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,47 @@
-(function ($, window, document, undefined) {
+(function($, window, document, undefined) {
 
   'use strict';
 
-  $(function () {
-    // FireShell
+  $(function() {
+
+
+    // Contact form
+    var $contactForm = $('#contact-form'),
+      $submitButton = $('#contact-form input[type=submit]');
+    $contactForm.submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        url: '//formspree.io/vanwijk.mc@gmail.com',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+          $submitButton.prop('disabled', true);
+          $contactForm.find('.alert.sent').remove();
+          $submitButton.val('Sending message...');
+        },
+        success: function() {
+          // $contactForm.find('.alert.sending').remove();
+          $submitButton.before('<div class="alert sent">Thank you, we will be in contact shortly.</div>');
+          $submitButton.val('Message sent!');
+          $submitButton.prop('disabled', false);
+          setTimeout(function() {
+            $submitButton.val('Send');
+          }, 5000);
+        },
+        error: function() {
+          $submitButton.before('<div class="alert error">Oops, there was an error. Please contact us directly at hello@gapbridge.co.za</div>');
+          $submitButton.val('Error!');
+          $submitButton.prop('disabled', false);
+          setTimeout(function() {
+            $submitButton.val('Send');
+            $contactForm.find('.alert.error').remove();
+          }, 10000);
+        }
+      });
+    });
+
+
   });
 
 })(jQuery, window, document);
